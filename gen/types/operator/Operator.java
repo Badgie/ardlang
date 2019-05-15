a051e99b4f14f2fc287f4769b3ecc46e65b5d7c3
Badgy <jonaskhansen@gmail.com>
2019-05-15 10:15:31 +0200

    add operator nodes

---
+++ gen/types/operator/Operator.java
@@ -0,0 +1,75 @@
+package types.operator;
+
+import types.AST;
+
+public abstract class Operator extends AST {
+    //arithmetics
+    public class Add extends Operator {
+
+    }
+
+    public class Sub extends Operator {
+
+    }
+
+    public class Mult extends Operator {
+
+    }
+
+    public class Div extends Operator {
+
+    }
+
+    public class Mod extends Operator {
+
+    }
+
+    public class Incr extends Operator {
+
+    }
+
+    public class Decr extends Operator {
+
+    }
+
+    public class Assign extends Operator {
+
+    }
+
+    //boolean
+    public class Equal extends Operator {
+
+    }
+
+    public class Greater extends Operator {
+
+    }
+
+    public class Lesser extends Operator {
+
+    }
+
+    public class GreaterEqual extends Operator {
+
+    }
+
+    public class LesserEqual extends Operator {
+
+    }
+
+    public class Or extends Operator {
+
+    }
+
+    public class Xor extends Operator {
+
+    }
+
+    public class And extends Operator {
+
+    }
+
+    public class Not extends Operator {
+
+    }
+}
