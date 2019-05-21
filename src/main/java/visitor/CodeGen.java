25f5826373c8ad57fdc7727a6e201b0691c6561c
Badgy <jonaskhansen@gmail.com>
2019-05-21 12:08:50 +0200

    create structure behavior in codegen

--- src/main/java/visitor/CodeGen.java
+++ src/main/java/visitor/CodeGen.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class CodeGen extends ASTVisitor {
     private StringBuilder code = new StringBuilder();
     private int taskCount = 0;
+    private List<StmtsTask> taskList;
 
     public String gen(AST node) {
         setupScheduler();
@@ -46,6 +47,8 @@ public class CodeGen extends ASTVisitor {
                 stmts.add(s);
             }
         }
+
+        taskList = tasks;
 
         for (StmtsTask s : tasks) {
             visit(s);
@@ -80,236 +83,287 @@ public class CodeGen extends ASTVisitor {
     @Override
     public void visit(StmtsFunc node) {
         visit((AST) node.getType());
+        code.append(node.getIdentifier())
+                .append("(");
+        for (Param p : node.getParams()) {
+            visit(p);
+        }
+        code.append(") {");
+        for (StmtsBlockStmts s : node.getStmts()) {
+            visit(s);
+        }
+        code.append("}");
     }
 
     @Override
     public void visit(StmtsBlockStmts node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(BlockStmtsDcl node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(BlockStmtsExpr node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(BlockStmtsStmt node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(IfStmt node) {
-
+        code.append("if (");
+        visit(node.getCondition());
+        code.append(") {");
+        for (StmtsBlockStmts s : node.getStmts()) {
+            visit(s);
+        }
+        code.append("}");
+        for (EifStmt e : node.getEifStmts()) {
+            visit(e);
+        }
     }
 
     @Override
     public void visit(EifStmt node) {
-
+        if (node.getCondition() != null) {
+            code.append("else if (");
+            visit(node.getCondition());
+            code.append(") {");
+        } else {
+            code.append("else {");
+        }
+        for (StmtsBlockStmts s : node.getStmts()) {
+            visit(s);
+        }
     }
 
     @Override
     public void visit(ForStmt node) {
-
+        code.append("for (");
+        if (node.getDcl() != null) {
+            visit(node.getDcl());
+        } else {
+            visit(node.getNum());
+        }
+        code.append(";");
+        visit(node.getCond());
+        code.append(";");
+        visit(node.getcExpr());
+        code.append(") {");
+        for (StmtsBlockStmts s : node.getStmts()) {
+            visit(s);
+        }
+        code.append("}");
     }
 
     @Override
     public void visit(FuncStmt node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(AssignExpr node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(BoolExpr node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(CalcExpr node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(BoolCondition node) {
-
+        if (node.getBoolExpr() != null) {
+            visit(node.getBoolExpr());
+        } else {
+            visit(node.getFuncStmt());
+        }
     }
 
     @Override
     public void visit(Param node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Val node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(ValBool node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(ValFunc node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(ValIdent node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(ValLiteral node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(ValString node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(ValNumber node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(NumberIval node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(NumberFval node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(NumberArrayIndex node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator node) {
-
+        super.visit((AST) node);
     }
 
     @Override
     public void visit(Operator.Decr node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Incr node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Assign node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.GreaterEqual node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Not node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Div node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Mult node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.LesserEqual node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Greater node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Sub node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Mod node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.And node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Or node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Add node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Lesser node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Xor node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Operator.Equal node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Type node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Type.TypeInt node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Type.TypeString node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Type.TypeDouble node) {
-
+        //TODO: implement ino behavior
     }
 
     @Override
     public void visit(Type.TypeBool node) {
+        //TODO: implement ino behavior
+    }
 
+    @Override
+    public void visit(Type.TypeVoid node) {
+        //TODO: implement ino behavior
     }
 
     @Override
